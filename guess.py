5522be555fb23842f76c14dcbe305b22fdb667b9
Peterko <oppment@yahoo.com>
2021-04-14 14:39:05 +0800

    update self define

--- guess.py
+++ guess.py
@@ -1,6 +1,7 @@
 import random
-
-r = random.randint(1,100)
+start = int(input('請輸入起始值'))
+end = int(input('請輸入結束值'))
+r = random.randint(start,end)
 count = 0
 while True:
 	count += 1 # count = count + 1
